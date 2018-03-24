8d2e03e8dd3b495015135037da50fb9ff1d38066
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2018-03-24 20:06:28 +0100

    Images can be displayes.
    - Added snake and apple images that are drawn on the screen.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,4 @@
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import java.awt.*;
 
@@ -6,6 +6,12 @@ public class Game extends JPanel {
 
     private int GAME_WINDOW_WIDTH = 400;
     private int GAME_WINDOW_HEIGHT = 400;
+
+
+    private ImageIcon snakeBodyIcon = new ImageIcon("Images/green.png");
+    private ImageIcon appleIcon = new ImageIcon("Images/red.png");
+    private Image snakeBodyImage = snakeBodyIcon.getImage();
+    private Image appleImage = appleIcon.getImage();
 
     public Game() {
         setBackground(Color.BLACK);
@@ -15,5 +21,10 @@ public class Game extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        g.drawImage(snakeBodyImage, 10, 10, this);
+        g.drawImage(snakeBodyImage, 20, 20, this);
+        g.drawImage(snakeBodyImage, 10, 20, this);
+        g.drawImage(appleImage, 100, 100, this);
     }
 }
